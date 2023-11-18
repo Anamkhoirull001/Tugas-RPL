5ee983a80844af85c939d97c62f9b00470d91ff7
mahardikasuryakusuma <mahardikasurya15@gmail.com>
2023-11-18 19:42:42 +0700

    praktikum3 belum belum lengkap

---
+++ Tugas Praktikum/Praktikum3/gentelella/production/setting.php
@@ -0,0 +1,23 @@
+<?php
+$connect = mysqli_connect("localhost", "root", "blekiimut", "rpl");
+$first_name = $_POST['first_name'];
+$middle_name = $_POST['middle_name'];
+$last_name = $_POST['last_name'];
+$gender = $_POST['gender'];
+$date_of_birth = $_POST['date_of_birth'];
+// $sql = "INSERT INTO `data` (`first_name`, `middle_name`, `last_name`, `gender`, 'date_of_birth') VALUES ( '$first_name', '$middle_name', '$last_name', '$gender', '$date_of_birth')";
+$sql = "INSERT INTO `data` (`first_name`, `middle_name`, `last_name`, `gender`, `date_of_birth`) VALUES ('$first_name', '$middle_name', '$last_name', '$gender', '$date_of_birth')";
+
+// insert in database 
+$rs = mysqli_query($connect, $sql);
+
+if ($rs) {
+    echo "Contact Records Inserted";
+    // header('Location: form_wizards.html'); 
+    header('refresh:5;url=form_wizards.html'); 
+    exit;
+} else {
+    echo "Error: " . mysqli_error($connect);
+}
+
+?>
